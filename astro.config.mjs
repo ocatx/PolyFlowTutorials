d8229c36483692fb671d95af15704103f0cb0ba1
ocatx <ocatxf@gmail.com>
2026-05-13 19:23:48 -0400

    Rename local folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://ocatx.github.io',   // ← Change to your username
-  base: '/PolyFlowTutorials',                                // Important for username.github.io
+  site: 'https://ocatx.github.io',
+  base: '/PolyFlowTutorials',
 });
