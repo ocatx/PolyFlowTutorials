daa58c3a1a1024ad45f427e2be36f9bd61f7bb4b
ocatx <ocatxf@gmail.com>
2026-05-13 18:40:57 -0400

    Force redeploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://xuefengzhao.github.io',   // ← Change to your username
   base: '/',                                // Important for username.github.io
-  // You can add more later
 });
